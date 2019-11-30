b52665ee362a6f8e672c407d81acb375689c3088
Johan Larsson <johan_gavle1@hotmail.com>
2019-11-30 20:18:20 +0100

    Simplify.

--- SelectOnFocusBox/TextBoxExt.cs
+++ SelectOnFocusBox/TextBoxExt.cs
@@ -3,30 +3,30 @@
     using System;
     using System.Diagnostics;
     using System.Windows;
+    using System.Windows.Controls;
     using System.Windows.Controls.Primitives;
+    using System.Windows.Input;
 
-    public static class TextBox
+    public static class TextBoxExt
     {
         public static readonly DependencyProperty SelectAllOnGotKeyboardFocusProperty = DependencyProperty.RegisterAttached(
             "SelectAllOnGotKeyboardFocus",
             typeof(bool),
-            typeof(TextBox),
+            typeof(TextBoxExt),
             new FrameworkPropertyMetadata(
                 false,
                 FrameworkPropertyMetadataOptions.Inherits));
 
-        static TextBox()
+        static TextBoxExt()
         {
+            EventManager.RegisterClassHandler(typeof(TextBox), UIElement.PreviewMouseLeftButtonDownEvent, new MouseButtonEventHandler(OnPreviewMouseLeftButtonDown));
             EventManager.RegisterClassHandler(typeof(TextBoxBase), UIElement.GotKeyboardFocusEvent, new RoutedEventHandler(Select));
             EventManager.RegisterClassHandler(typeof(TextBoxBase), UIElement.MouseUpEvent, new RoutedEventHandler(Dump), handledEventsToo: true);
-            EventManager.RegisterClassHandler(typeof(TextBoxBase), UIElement.PreviewMouseUpEvent, new RoutedEventHandler(Dump), handledEventsToo: true);
+            EventManager.RegisterClassHandler(typeof(TextBoxBase), UIElement.PreviewMouseUpEvent, new RoutedEventHandler(MouseUp), handledEventsToo: true);
             EventManager.RegisterClassHandler(typeof(TextBoxBase), TextBoxBase.SelectionChangedEvent, new RoutedEventHandler(Dump), handledEventsToo: true);
             EventManager.RegisterClassHandler(typeof(TextBoxBase), UIElement.GotFocusEvent, new RoutedEventHandler(Dump), handledEventsToo: true);
         }
 
-        /// <summary>Helper for setting <see cref="SelectAllOnGotKeyboardFocusProperty"/> on <paramref name="element"/>.</summary>
-        /// <param name="element"><see cref="UIElement"/> to set <see cref="SelectAllOnGotKeyboardFocusProperty"/> on.</param>
-        /// <param name="value">SelectAllOnGotKeyboardFocus property value.</param>
         public static void SetSelectAllOnGotKeyboardFocus(this UIElement element, bool value)
         {
             if (element is null)
@@ -37,11 +37,6 @@
             element.SetValue(SelectAllOnGotKeyboardFocusProperty, value);
         }
 
-        /// <summary>Helper for getting <see cref="SelectAllOnGotKeyboardFocusProperty"/> from <paramref name="element"/>.</summary>
-        /// <param name="element"><see cref="UIElement"/> to read <see cref="SelectAllOnGotKeyboardFocusProperty"/> from.</param>
-        /// <returns>SelectAllOnGotKeyboardFocus property value.</returns>
-        [AttachedPropertyBrowsableForChildren(IncludeDescendants = false)]
-        [AttachedPropertyBrowsableForType(typeof(UIElement))]
         public static bool GetSelectAllOnGotKeyboardFocus(this UIElement element)
         {
             if (element is null)
@@ -52,8 +47,6 @@
             return (bool)element.GetValue(SelectAllOnGotKeyboardFocusProperty);
         }
 
-
-
         private static void Select(object sender, RoutedEventArgs e)
         {
             Dump(e);
@@ -61,6 +54,27 @@
                 textBoxBase.GetSelectAllOnGotKeyboardFocus())
             {
                 textBoxBase.SelectAll();
+            }
+        }
+
+        private static void OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Dump(e);
+            if (sender is TextBoxBase { IsKeyboardFocusWithin: false, IsEnabled: true, Focusable: true } textBoxBase &&
+                textBoxBase.GetSelectAllOnGotKeyboardFocus() &&
+                textBoxBase.Focus())
+            {
+                e.Handled = true;
+            }
+        }
+
+        private static void MouseUp(object sender, RoutedEventArgs e)
+        {
+            Dump(e);
+            if (sender is TextBoxBase textBoxBase &&
+                textBoxBase.GetSelectAllOnGotKeyboardFocus())
+            {
+                //textBoxBase.EndChange();
             }
         }
 
@@ -73,7 +87,7 @@
         {
             if (e.Source is System.Windows.Controls.TextBox source)
             {
-                Debug.WriteLine($"IsFocused: {source.IsFocused,-5} IsKeyboardFocused: {source.IsKeyboardFocused,-5} IsKeyboardFocusWithin: {source.IsKeyboardFocusWithin,-5} IsSelectionActive: {source.IsSelectionActive,-5} SelectedText: {source.SelectedText,-5} {e.RoutedEvent.Name}");
+                Debug.WriteLine($"IsFocused: {source.IsFocused,-5} IsKeyboardFocused: {source.IsKeyboardFocused,-5} SelectedText: {source.SelectedText,-5} {e.RoutedEvent.Name}");
             }
         }
     }
